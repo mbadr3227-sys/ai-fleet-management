5400542685e2e106befafd3eebda6d5ea029d95e
Mohamed Badr <mbadr3227@gmail.com>
2026-08-18 03:58:24 +0200

    Prepare for deployment

--- app.py
+++ app.py
@@ -455,7 +455,7 @@ def setup_database():
 
     conn.commit()
     conn.close()
+setup_database()
 
 if __name__ == "__main__":
-    setup_database()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
